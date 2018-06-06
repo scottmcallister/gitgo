da425b7a55d3bfb66ca73ece211352b0aaf1a222
Scott McAllister <mr.scott.mcallister@gmail.com>
2018-06-05 23:28:33 -0300

    fetch user info and display

--- main.go
+++ main.go
@@ -16,16 +16,28 @@ func main() {
 	flag.Parse()
 
 	if flag.NFlag() == 0 {
-		fmt.Printf("Usage: %s [options]\n", os.Args[0])
-		fmt.Println("Options:")
-		flag.PrintDefaults()
-		os.Exit(1)
+		printUsage()
 	}
 
 	users := strings.Split(user, ",")
 	fmt.Printf("Searching user(s): %s\n", users)
+
+	for _, u := range users {
+		result := getUsers(u)
+		fmt.Println(`Usernam:  `, result.Login)
+		fmt.Println(`Name:     `, result.Name)
+		fmt.Println(`Location:     `, result.Location)
+		fmt.Println(`Email:      `, result.Email)
+	}
 }
 
 func init() {
 	flag.StringVarP(&user, "user", "u", "", "Search Users")
+}
+
+func printUsage() {
+	fmt.Printf("Usage: %s [options]\n", os.Args[0])
+	fmt.Println("Options:")
+	flag.PrintDefaults()
+	os.Exit(1)
 }
