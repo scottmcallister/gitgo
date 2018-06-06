a8733e466bdb757d36dc820c9a4aec6e10e63520
Scott McAllister <mr.scott.mcallister@gmail.com>
2018-06-05 23:10:26 -0300

    add flag for passing in a username to search

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	flag "github.com/ogier/pflag"
+)
+
+var (
+	user string
+)
 
 func main() {
-	fmt.Println("Hello World")
+	flag.Parse()
+
+	if flag.NFlag() == 0 {
+		fmt.Printf("Usage: %s [options]\n", os.Args[0])
+		fmt.Println("Options:")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	users := strings.Split(user, ",")
+	fmt.Printf("Searching user(s): %s\n", users)
+}
+
+func init() {
+	flag.StringVarP(&user, "user", "u", "", "Search Users")
 }
